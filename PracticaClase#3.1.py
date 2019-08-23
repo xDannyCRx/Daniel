a8873c2c1256fb34092b416da91f106477a5e40e
Clase 3 <jdaz25@gmail.com>
2019-08-22 20:34:07 -0600

    practica final lession 3

--- PracticaClase#3.1.py
+++ PracticaClase#3.1.py
@@ -8,14 +8,33 @@
 #$olicite al usuario ingresar los datos de un nuevo empleado o ver los empleados que hay.
 
 import sqlite3
-conexion1=sqlite3.connect('bd.empleados')
-conexion2=sqlite3.connect('db.transacciones')
+conexion1=sqlite3.connect('empleados.db')
+conexion2=sqlite3.connect('transacciones.db')
 try:
-    conexion1.execute('''create table empleados(cedulas integer primary key, nombre test, cedula text, puesto text)''')
+    conexion1.execute('''create table empleados(cedulas integer primary key AUTOINCREMENT, nombre test, cedula text, puesto text)''')
     conexion1.execute('''create table Horarios''')
-    conexion2.execute('''create table inventario(codigo integer primary key AUTOINCREMENT, entrada test, salida text, saldo real)''')
+    conexion2.execute('''create table transacciones(inventario integer primary key AUTOINCREMENT, entrada test, salida text, saldo text)''')
     conexion2.execute('''create table efectivo''')
     conexion2.execute('''create table activos''')
     print('se crea la tabla')
 except sqlite3.OperationalError:
     print('Ya existo')
+
+nombre=(input('Digite el nombre del empleado = \n')) 
+cedula=(input('Digite la cedula = \n')) 
+puesto=(input('Digite el puesto = \n')) 
+conexion1.execute('insert into empleados(nombre, cedula, puesto )values(?,?,?)',(nombre,cedula,puesto))
+conexion1.commit()
+curso=conexion1.execute('select cedulas,nombre,puesto from empleados')
+
+entrada=(input('Digite el Articulo de ingresar = \n')) 
+salida=(input('Digite la cantidad de Articulos de salida= \n')) 
+saldo=(input('Digite el Saldo a pagar = \n')) 
+conexion2.execute('insert into transacciones(entrada, salida, saldo )values(?,?,?)',(entrada,salida,saldo))
+conexion2.commit()
+curso=conexion2.execute('select inventario,entrada,salida from transacciones')
+
+for fila in curso:
+    print(fila)
+conexion1.close() 
+conexion2.close() 
